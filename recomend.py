29fa87013da2d4eb3576157e3ba4cd703008de3d
truoqu01 <quy_nguyen88@yahoo.com>
2023-09-11 10:40:53 +0700

    add.hello

--- recomend.py
+++ recomend.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 from sklearn.feature_extraction.text import TfidfVectorizer
